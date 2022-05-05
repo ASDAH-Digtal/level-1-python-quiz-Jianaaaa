7de8239b00f1b69e0dd0fce5d65ef844be281e47
Jianaaaa <jianamari_vin@student.asdah.school.nz>
2022-05-05 14:32:40 +1200

    moved files here

---
+++ main/quiz.py
@@ -0,0 +1,108 @@
+print("General knowledge quiz (Disney Movies)")
+#u forgot to ask for their name
+print("Hello!")
+name = input("What's your name?\n>> ")
+print("Good luck " + name + "!")
+print("You currently have 0 points. Try to get 100 points by the end of this quiz :)")
+#Insert question 1 (q1=input(1.___)*add options*)
+q1 = input("\n1. Who is the main character in the disney movie’s game ‘Wreck-it Ralph’?\na) Ralph\nb) Q-bert\nc) Felix\nd) Sonic\n>> ")
+
+if q1 == "Felix" or q1 == "felix" or q1 == "c":
+    print("Correct! \n10 points have been added")
+elif q1 == "a" or q1 == "b" or q1 == "d" or q1 == "ralph" or q1 == "q-bert" or q1 == "sonic":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+
+#2. ""print("Correct! \n10 points have been added")"" how do i add the scoring featurrreeee
+q2 = input("\n2. Who was the villain in ‘Frozen 1’ movie?\na) Christoff\nb) Hans\nc) Elsa\nd) Duke Weselton\n>> ")
+
+if q2 == "Hans" or q2 == "hans" or q2 == "b":
+    print("Correct! \n10 points have been added")
+elif q2 == "a" or q2 == "c" or q2 == "d" or q2 == "Christoff" or q2 == "Elsa" or q2 == "Duke Weselton":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+    
+#3
+q3 = input("\n3. How many dwarfs were in the movie ‘Snow White’?\na) 9\nb) 10\nc) 8\nd) 7\n>> ")
+
+if q3 == "7" or q3 == "d":
+    print("Correct! \n10 points have been added")
+elif q3 == "9" or q3 == "10" or q3 == "8" or q3 == "a" or q3 == "b" or q3 == "c":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+
+#4
+q4 = input("\n4. What type of animal is goofy from ‘Mickey Mouse’?\na) dog\nb) cow\nc) mouse\nd) duck\n>> ")
+
+if q4 == "dog" or q4 == "a":
+    print("Correct! \n10 points have been added")
+elif q4 == "b" or q4 == "c" or q4 == "d" or q4 == "cow" or q4 == "mouse" or q4 == "duck":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+    
+#5
+q5 = input("\n5. What alias does Mulan use when she went to war training?\na) Fa Jun\nb) Hua Mulan\nc) Fa Ping\nd) Jun Ping\n>> ")
+
+if q5 == "Fa Ping" or q5 == "fa ping" or q5 == "c":
+    print("Correct! \n10 points have been added")
+elif q5 == "b" or q5 == "a" or q5 == "d" or q5 == "Fa Jun" or q5 == "Hua Mulan" or q5 == "Jun Ping":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+
+#6
+q6 = input("\n6. Who was the bully from the movie ‘Luca’?\na) Paguro\nb) Ercole\nc) Lorenzo\nd) Guido\n>> ")
+
+if q6 == "Ercole" or q6 == "ercole" or q6 == "b":
+    print("Correct! \n10 points have been added")
+elif q6 == "a" or q6 == "c" or q6 == "d" or q6 == "Paguro" or q6 == "Lorenzo" or q6 == "duck":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+
+#7
+q7 = input("\n7. Ariel is the _th daughter of King Triton\na) 6\nb) 7\nc) 13\nd) 11\n>> ")
+
+if q7 == "7" or q7 == "b":
+    print("Correct! \n10 points have been added")
+elif q7 == "a" or q7 == "c" or q7 == "d" or q7 == "6" or q7 == "13" or q7 == "11":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+
+#8
+q8 = input("\n8. What was the alligators name from 'The princess and the frog'?\na) Marlon\nb) Buford\nc) James\nd) Louis\n>> ")
+
+if q8 == "Louis" or q8 == "d" or q8 == "louis":
+    print("Correct! \n10 points have been added")
+elif q8 == "a" or q8 == "c" or q8 == "b" or q8 == "Marlon" or q8 == "Buford" or q8 == "James":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+
+#9
+q9 = input("\n9. What was Aladdin's first wish?\na) “I wish the princess would fall in love with me”\nb) “I wish for money”\nc) “I wish to be a prince”\nd) “I wish to be a Sultan”\n>> ")
+
+if q9 == "c" or q9 == "“I wish to be a prince”" or q9 == "I wish to be a prince":
+    print("Correct! \n10 points have been added")
+elif q9 == "a" or q9 == "d" or q9 == "b" or q9 == "I wish the princess would fall in love with me" or q9 == "I wish for money" or q9 == "I wish to be a Sultan":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+
+#10
+q10 = input("\n10. Who assisted Hercules throughout the movie?\na) Nessus\nb) Meg\nc) Philoctetes\nd) Hermes\n>> ")
+
+if q10 == "Philoctetes" or q10 == "c" or q10 == "philoctetes":
+    print("Correct! \n10 points have been added")
+elif q10 == "a" or q10 == "d" or q10 == "b" or q10 == "Meg" or q10 == "Hermes" or q10 == "Nessus":
+    print("Too bad, that's incorrect")
+else:
+    print("That's an invalid answer")
+
+
+
---
+++ main/tutorial_one.py
@@ -0,0 +1,32 @@
+# 1. Comment
+
+# 2. Print statement
+print("Kia ora, world!")
+print("Hello there")
+print("1, 2, 3")
+print(1+2)
+print("1+2")
+# 3. Types 
+# intergers, whole numbers, e.g. 1, 0, 100, -20
+# Strings, text, words, numbers, characters, inside quotation marks, ""
+# Floats: numbers with decimals, e.g. 2.0, 10.4
+
+#4. Variables 
+name = "Jiana"
+print(name)
+print("Kia ora " + name)
+
+# 5. Input
+age = input("Enter age: ")
+print("Oh, you're {} years old?".format(age))
+
+age = int(age)
+print(type(age))
+
+# 6. If-and-else statements
+if age < 5 and age > 0:
+    print("You are a baby")
+elif age < 20 and age :
+    print("You must be in school")
+elif age < 100:
+    print("You are old")
