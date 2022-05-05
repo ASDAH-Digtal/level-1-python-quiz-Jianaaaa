754ebbc7b66d11d3f3b2c880992728ad20f575b0
Jianaaaa <jianamari_vin@student.asdah.school.nz>
2022-05-05 15:15:36 +1200

    addded points thing

--- main/quiz.py
+++ main/quiz.py
@@ -4,11 +4,13 @@ print("Hello!")
 name = input("What's your name?\n>> ")
 print("Good luck " + name + "!")
 print("You currently have 0 points. Try to get 100 points by the end of this quiz :)")
+total_point = 0
 #Insert question 1 (q1=input(1.___)*add options*)
 q1 = input("\n1. Who is the main character in the disney movie’s game ‘Wreck-it Ralph’?\na) Ralph\nb) Q-bert\nc) Felix\nd) Sonic\n>> ")
 
 if q1 == "Felix" or q1 == "felix" or q1 == "c":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q1 == "a" or q1 == "b" or q1 == "d" or q1 == "ralph" or q1 == "q-bert" or q1 == "sonic":
     print("Too bad, that's incorrect")
 else:
@@ -19,6 +21,7 @@ q2 = input("\n2. Who was the villain in ‘Frozen 1’ movie?\na) Christoff\nb) 
 
 if q2 == "Hans" or q2 == "hans" or q2 == "b":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q2 == "a" or q2 == "c" or q2 == "d" or q2 == "Christoff" or q2 == "Elsa" or q2 == "Duke Weselton":
     print("Too bad, that's incorrect")
 else:
@@ -29,6 +32,7 @@ q3 = input("\n3. How many dwarfs were in the movie ‘Snow White’?\na) 9\nb) 1
 
 if q3 == "7" or q3 == "d":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q3 == "9" or q3 == "10" or q3 == "8" or q3 == "a" or q3 == "b" or q3 == "c":
     print("Too bad, that's incorrect")
 else:
@@ -39,6 +43,7 @@ q4 = input("\n4. What type of animal is goofy from ‘Mickey Mouse’?\na) dog\n
 
 if q4 == "dog" or q4 == "a":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q4 == "b" or q4 == "c" or q4 == "d" or q4 == "cow" or q4 == "mouse" or q4 == "duck":
     print("Too bad, that's incorrect")
 else:
@@ -49,6 +54,7 @@ q5 = input("\n5. What alias does Mulan use when she went to war training?\na) Fa
 
 if q5 == "Fa Ping" or q5 == "fa ping" or q5 == "c":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q5 == "b" or q5 == "a" or q5 == "d" or q5 == "Fa Jun" or q5 == "Hua Mulan" or q5 == "Jun Ping":
     print("Too bad, that's incorrect")
 else:
@@ -59,6 +65,7 @@ q6 = input("\n6. Who was the bully from the movie ‘Luca’?\na) Paguro\nb) Erc
 
 if q6 == "Ercole" or q6 == "ercole" or q6 == "b":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q6 == "a" or q6 == "c" or q6 == "d" or q6 == "Paguro" or q6 == "Lorenzo" or q6 == "duck":
     print("Too bad, that's incorrect")
 else:
@@ -69,6 +76,7 @@ q7 = input("\n7. Ariel is the _th daughter of King Triton\na) 6\nb) 7\nc) 13\nd)
 
 if q7 == "7" or q7 == "b":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q7 == "a" or q7 == "c" or q7 == "d" or q7 == "6" or q7 == "13" or q7 == "11":
     print("Too bad, that's incorrect")
 else:
@@ -79,6 +87,7 @@ q8 = input("\n8. What was the alligators name from 'The princess and the frog'?\
 
 if q8 == "Louis" or q8 == "d" or q8 == "louis":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q8 == "a" or q8 == "c" or q8 == "b" or q8 == "Marlon" or q8 == "Buford" or q8 == "James":
     print("Too bad, that's incorrect")
 else:
@@ -89,6 +98,7 @@ q9 = input("\n9. What was Aladdin's first wish?\na) “I wish the princess would
 
 if q9 == "c" or q9 == "“I wish to be a prince”" or q9 == "I wish to be a prince":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q9 == "a" or q9 == "d" or q9 == "b" or q9 == "I wish the princess would fall in love with me" or q9 == "I wish for money" or q9 == "I wish to be a Sultan":
     print("Too bad, that's incorrect")
 else:
@@ -99,10 +109,11 @@ q10 = input("\n10. Who assisted Hercules throughout the movie?\na) Nessus\nb) Me
 
 if q10 == "Philoctetes" or q10 == "c" or q10 == "philoctetes":
     print("Correct! \n10 points have been added")
+    total_point += 10
 elif q10 == "a" or q10 == "d" or q10 == "b" or q10 == "Meg" or q10 == "Hermes" or q10 == "Nessus":
     print("Too bad, that's incorrect")
 else:
     print("That's an invalid answer")
 
 
-
+print("You got a total score of {}".format(total_point))
