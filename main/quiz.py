895d2d836d017a798df1a581a88f6ff04924a1c9
Jianaaaa <jianamari_vin@student.asdah.school.nz>
2022-05-11 13:38:25 +1200

    editted answer result

--- main/quiz.py
+++ main/quiz.py
@@ -6,9 +6,9 @@ print("Good luck " + name + "!")
 print("You currently have 0 points. Try to get 100 points by the end of this quiz :)")
 total_point = 0
 #Insert question 1 (q1=input(1.___)*add options*)
-q1 = input("\n1. Who is the main character in the disney movie’s game ‘Wreck-it Ralph’?\na) Ralph\nb) Q-bert\nc) Felix\nd) Sonic\n>> ")
+q1 = input("\n1. Who is the main character in the disney movie’s game ‘Wreck-it Ralph’?\na) Ralph\nb) Q-bert\nc) Felix\nd) Sonic\n>> ").lower()
 
-if q1 == "Felix" or q1 == "felix" or q1 == "c":
+if q1 == "felix" or q1 == "c":
     print("Correct! \n10 points have been added")
     total_point += 10
 elif q1 == "a" or q1 == "b" or q1 == "d" or q1 == "ralph" or q1 == "q-bert" or q1 == "sonic":
@@ -17,9 +17,9 @@ else:
     print("That's an invalid answer")
 
 #2. ""print("Correct! \n10 points have been added")"" how do i add the scoring featurrreeee
-q2 = input("\n2. Who was the villain in ‘Frozen 1’ movie?\na) Christoff\nb) Hans\nc) Elsa\nd) Duke Weselton\n>> ")
+q2 = input("\n2. Who was the villain in ‘Frozen 1’ movie?\na) Christoff\nb) Hans\nc) Elsa\nd) Duke Weselton\n>> ").lower()
 
-if q2 == "Hans" or q2 == "hans" or q2 == "b":
+if  q2 == "hans" or q2 == "b":
     print("Correct! \n10 points have been added")
     total_point += 10
 elif q2 == "a" or q2 == "c" or q2 == "d" or q2 == "Christoff" or q2 == "Elsa" or q2 == "Duke Weselton":
@@ -28,7 +28,7 @@ else:
     print("That's an invalid answer")
     
 #3
-q3 = input("\n3. How many dwarfs were in the movie ‘Snow White’?\na) 9\nb) 10\nc) 8\nd) 7\n>> ")
+q3 = input("\n3. How many dwarfs were in the movie ‘Snow White’?\na) 9\nb) 10\nc) 8\nd) 7\n>> ").lower()
 
 if q3 == "7" or q3 == "d":
     print("Correct! \n10 points have been added")
@@ -39,7 +39,7 @@ else:
     print("That's an invalid answer")
 
 #4
-q4 = input("\n4. What type of animal is goofy from ‘Mickey Mouse’?\na) dog\nb) cow\nc) mouse\nd) duck\n>> ")
+q4 = input("\n4. What type of animal is goofy from ‘Mickey Mouse’?\na) dog\nb) cow\nc) mouse\nd) duck\n>> ").lower()
 
 if q4 == "dog" or q4 == "a":
     print("Correct! \n10 points have been added")
@@ -50,9 +50,9 @@ else:
     print("That's an invalid answer")
     
 #5
-q5 = input("\n5. What alias does Mulan use when she went to war training?\na) Fa Jun\nb) Hua Mulan\nc) Fa Ping\nd) Jun Ping\n>> ")
+q5 = input("\n5. What alias does Mulan use when she went to war training?\na) Fa Jun\nb) Hua Mulan\nc) Fa Ping\nd) Jun Ping\n>> ").lower()
 
-if q5 == "Fa Ping" or q5 == "fa ping" or q5 == "c":
+if  q5 == "fa ping" or q5 == "c":
     print("Correct! \n10 points have been added")
     total_point += 10
 elif q5 == "b" or q5 == "a" or q5 == "d" or q5 == "Fa Jun" or q5 == "Hua Mulan" or q5 == "Jun Ping":
@@ -61,9 +61,9 @@ else:
     print("That's an invalid answer")
 
 #6
-q6 = input("\n6. Who was the bully from the movie ‘Luca’?\na) Paguro\nb) Ercole\nc) Lorenzo\nd) Guido\n>> ")
+q6 = input("\n6. Who was the bully from the movie ‘Luca’?\na) Paguro\nb) Ercole\nc) Lorenzo\nd) Guido\n>> ").lower()
 
-if q6 == "Ercole" or q6 == "ercole" or q6 == "b":
+if  q6 == "ercole" or q6 == "b":
     print("Correct! \n10 points have been added")
     total_point += 10
 elif q6 == "a" or q6 == "c" or q6 == "d" or q6 == "Paguro" or q6 == "Lorenzo" or q6 == "duck":
@@ -72,7 +72,7 @@ else:
     print("That's an invalid answer")
 
 #7
-q7 = input("\n7. Ariel is the _th daughter of King Triton\na) 6\nb) 7\nc) 13\nd) 11\n>> ")
+q7 = input("\n7. Ariel is the _th daughter of King Triton\na) 6\nb) 7\nc) 13\nd) 11\n>> ").lower()
 
 if q7 == "7" or q7 == "b":
     print("Correct! \n10 points have been added")
@@ -83,9 +83,9 @@ else:
     print("That's an invalid answer")
 
 #8
-q8 = input("\n8. What was the alligators name from 'The princess and the frog'?\na) Marlon\nb) Buford\nc) James\nd) Louis\n>> ")
+q8 = input("\n8. What was the alligators name from 'The princess and the frog'?\na) Marlon\nb) Buford\nc) James\nd) Louis\n>> ").lower()
 
-if q8 == "Louis" or q8 == "d" or q8 == "louis":
+if q8 == "d" or q8 == "louis":
     print("Correct! \n10 points have been added")
     total_point += 10
 elif q8 == "a" or q8 == "c" or q8 == "b" or q8 == "Marlon" or q8 == "Buford" or q8 == "James":
@@ -94,7 +94,7 @@ else:
     print("That's an invalid answer")
 
 #9
-q9 = input("\n9. What was Aladdin's first wish?\na) “I wish the princess would fall in love with me”\nb) “I wish for money”\nc) “I wish to be a prince”\nd) “I wish to be a Sultan”\n>> ")
+q9 = input("\n9. What was Aladdin's first wish?\na) “I wish the princess would fall in love with me”\nb) “I wish for money”\nc) “I wish to be a prince”\nd) “I wish to be a Sultan”\n>> ").lower()
 
 if q9 == "c" or q9 == "“I wish to be a prince”" or q9 == "I wish to be a prince":
     print("Correct! \n10 points have been added")
@@ -105,9 +105,9 @@ else:
     print("That's an invalid answer")
 
 #10
-q10 = input("\n10. Who assisted Hercules throughout the movie?\na) Nessus\nb) Meg\nc) Philoctetes\nd) Hermes\n>> ")
+q10 = input("\n10. Who assisted Hercules throughout the movie?\na) Nessus\nb) Meg\nc) Philoctetes\nd) Hermes\n>> ").lower()
 
-if q10 == "Philoctetes" or q10 == "c" or q10 == "philoctetes":
+if q10 == "c" or q10 == "philoctetes":
     print("Correct! \n10 points have been added")
     total_point += 10
 elif q10 == "a" or q10 == "d" or q10 == "b" or q10 == "Meg" or q10 == "Hermes" or q10 == "Nessus":
